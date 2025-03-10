6fce7cf12e36ad041607d9c277da05d4e0c1c350
hz-uiw <kwonmc5.11@gmail.com>
2025-03-10 12:26:11 +0900

    global css 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Global } from '@emotion/react'
+import { global } from "./styles/global"
 
 function App() {
     return (
     <>
-      
+      <Global styles={global}>
+      </Global>
     </>
   )
 }
