d70fa018e82a6cf5d9e795c647ead2ce24a02ba2
hz-uiw <kwonmc5.11@gmail.com>
2025-03-14 15:54:51 +0900

    product page 중간저장

--- src/pages/adminPages/AdminProductPage/AdminProductPage.jsx
+++ src/pages/adminPages/AdminProductPage/AdminProductPage.jsx
@@ -1,11 +1,31 @@
 /**@jsxImportSource @emotion/react */
+import { MdInfoOutline, MdOutlineCategory, MdProductionQuantityLimits } from 'react-icons/md';
 import * as s from './style';
 import React from 'react';
 
 function AdminProductPage(props) {
     return (
-        <div>
-            
+        <div css={s.mainContainer}>
+            <div css={s.mainLayout}>
+                <div css={s.conBox}>
+                    <div><h2>제품관리</h2></div>
+                    <div css={s.mainCon}>
+                        <div>
+                            <MdProductionQuantityLimits/>
+                            <h6>상품 관리</h6>
+                            
+                        </div>
+                        <div>
+                            <MdOutlineCategory />
+                            <h6>카테고리 관리</h6>
+                        </div>
+                        <div>
+                            <MdInfoOutline />
+                            <h6>영양정보/원산지 관리</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
