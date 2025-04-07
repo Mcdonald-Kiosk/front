6ee4a7f1b31ed5b10024ffb2aa65326597fdc14b
sojeong5448 <cleo708028@gmail.com>
2025-04-07 15:39:55 +0900

    oauth 로그인

--- src/pages/adminPages/LoginPage/LoginPage.jsx
+++ src/pages/adminPages/LoginPage/LoginPage.jsx
@@ -89,7 +89,9 @@ function LoginPage(props) {
 
     const passwordInputRef = React.useRef(null);
 
-
+    const handleOAuth2LoginOnClick = (provider) => {
+        window.location.href = `http://localhost:8080/oauth2/authorization/${provider}`;
+    }
 
     return (
         <div css={s.container}>
@@ -121,6 +123,14 @@ function LoginPage(props) {
                             <Link to={"/admin/join"}>회원가입</Link>
                         </p>
                         <button css={s.accountButton} onClick={handleLoginOnClick}>로그인</button>
+                    </div>
+                </div>
+                <div css={s.socialBox}>
+                    <h3 css={s.socialLoginTitle}>간편 로그인</h3>
+                    <div css={s.socialLoginImege}>
+                        <img onClick={() => handleOAuth2LoginOnClick("google")} src="https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/5rH/image/aFrEyVpANu07FvoBZQbIB4aF_uc"  alt="Google" />
+                        <img onClick={() => handleOAuth2LoginOnClick("naver")} src="https://i.namu.wiki/i/p_1IEyQ8rYenO9YgAFp_LHIAW46kn6DXT0VKmZ_jKNijvYth9DieYZuJX_E_H_4GkCER_sVKhMqSyQYoW94JKA.svg" alt="Naver" />
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/e/e3/KakaoTalk_logo.svg" alt="Kakao" />
                     </div>
                 </div>
             </div>
